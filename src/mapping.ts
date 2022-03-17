7f4e78a98c587347cab33c0ba2b92772a0eeedeb
nmimran99 <nmimran99@gmail.com>
2022-03-17 00:09:05 -0400

    master

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,45 +1,109 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import { Pool, Upgraded } from "../generated/Pool/Pool"
-import { ExampleEntity } from "../generated/schema"
+import {
+  Borrow,
+  IsolationModeTotalDebtUpdated,
+  RebalanceStableBorrowRate,
+  Repay,
+  SwapBorrowRateMode
+} from "../generated/Pool/Pool"
+import { BorrowEvent, Account, Reserve, RepayEvent } from "../generated/schema"
+import { createAccount, updateCommonATokenStats, createReserve } from "./helpers";
 
-export function handleUpgraded(event: Upgraded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleBorrow(event: Borrow): void {
+  
+  let reserve = Reserve.load(event.address.toHexString());
+  if (reserve == null) {
+    reserve = createReserve(event.address.toHexString());
+  }
+  if (reserve == null) return;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let accountID = event.params.user.toHex();
+  let account = Account.load(accountID);
+  if (account == null) {
+    account = createAccount(accountID);
+  }
+  account.hasBorrowed = true;
+  account.save();
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let aTokenStats = updateCommonATokenStats(
+    reserve.id,
+    accountID,
+    event.transaction.hash,
+    event.block.timestamp,
+    event.block.number,
+    event.logIndex
+  )
+  
+  aTokenStats.totalUnderlyingBorrowed = aTokenStats.totalUnderlyingBorrowed
+    .plus(event.params.amount.toBigDecimal());
+
+  aTokenStats.save()
+
+  let borrowId = event.transaction.hash.toHexString();
+
+  let borrowAmount = event.params.amount.toBigDecimal();
+  let borrow = new BorrowEvent(borrowId);
+  borrow.reserve = event.address
+  borrow.borrower = event.params.user;
+  borrow.onBehalfOf = event.params.onBehalfOf;
+  borrow.amount = borrowAmount;
+  borrow.borrowRate = event.params.borrowRate;
+  borrow.interestRateMode = event.params.interestRateMode;
+  borrow.blockNumber = event.block.number.toI32();
+  borrow.blockTime = event.block.timestamp.toI32();
+  borrow.save();
+}
+
+export function handleIsolationModeTotalDebtUpdated(
+  event: IsolationModeTotalDebtUpdated
+): void {}
+
+export function handleRebalanceStableBorrowRate(
+  event: RebalanceStableBorrowRate
+): void {}
+
+export function handleRepay(event: Repay): void {
+  let reserve = Reserve.load(event.address.toHexString());
+  if (reserve == null) {
+    reserve = createReserve(event.address.toHexString());
+  }
+  let accountID = event.params.user.toHex();
+  let account = Account.load(accountID);
+  if (account == null) {
+    account = createAccount(accountID);
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let aTokenStats = updateCommonATokenStats(
+    reserve.id,
+    accountID,
+    event.transaction.hash,
+    event.block.timestamp,
+    event.block.number,
+    event.logIndex
+  )
 
-  // Entity fields can be set based on event parameters
-  entity.implementation = event.params.implementation
+  aTokenStats.totalUnderlyingRepaid = aTokenStats.totalUnderlyingRepaid
+    .plus(event.params.amount.toBigDecimal())
+  
+  aTokenStats.save()
+  
+  let repayID = event.transaction.hash
+  .toHex()
+  .concat('-')
+  .concat(event.transactionLogIndex.toString())
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  let repayAmount = event.params.amount.toBigDecimal();
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.admin(...)
-  // - contract.implementation(...)
+  let repay = new RepayEvent(repayID)
+  repay.reserve = event.address
+  repay.amount = repayAmount
+  repay.borrower = event.params.user
+  repay.repayer = event.params.repayer
+  repay.useATokens = event.params.useATokens
+  repay.blockTime = event.block.timestamp.toI32()
+  repay.blockNumber = event.block.number.toI32()
+  
+  repay.save()
 }
+
+export function handleSwapBorrowRateMode(event: SwapBorrowRateMode): void {}
